b492a064a26ea41e0fa1e69d8f222a4129e0462f
tokatu4561 <ryobsk4561@gmail.com>
2023-09-10 14:43:50 +0900

    show turns bord

--- public/main.js
+++ public/main.js
@@ -1,57 +1,79 @@
-const EMPTY = 0
-const DARK = 1
-const LIGHT = 2
+const EMPTY = 0;
+const DARK = 1;
+const LIGHT = 2;
 
-const INITIAL_BOARD = [
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, DARK, LIGHT, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, LIGHT, DARK, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY],
-  [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-]
-
-const boardElement = document.getElementById('board')
+const boardElement = document.getElementById("board");
 
 async function showBoard() {
+  const turnCount = 0;
+  const response = await fetch(`/api/games/latest/turns/${turnCount}`);
+  const responseBody = await response.json();
+  const board = responseBody.board;
+  const nextDisc = responseBody.nextDisc;
+
   // 盤面初期化 (子要素を全て削除)
   while (boardElement.firstChild) {
-    boardElement.removeChild(boardElement.firstChild)
+    boardElement.removeChild(boardElement.firstChild);
   }
 
   // 盤面描画
-  INITIAL_BOARD.forEach((line) => {
+  board.forEach((line) => {
     line.forEach((square) => {
       // <div class="square">
-      const squareElement = document.createElement('div')
-      squareElement.className = 'square'
+      const squareElement = document.createElement("div");
+      squareElement.className = "square";
 
       if (square !== EMPTY) {
         // <div class="stone dark">
-        const stoneElement = document.createElement('div')
-        const color = square === DARK ? 'dark' : 'light'
-        stoneElement.className = `stone ${color}`
+        const stoneElement = document.createElement("div");
+        const color = square === DARK ? "dark" : "light";
+        stoneElement.className = `stone ${color}`;
 
-        squareElement.appendChild(stoneElement)
+        squareElement.appendChild(stoneElement);
+      } else {
+        // 置ける場所にはクリックイベントを設定
+        squareElement.addEventListener("click", async () => {
+          const nextTurnCount = turnCount + 1;
+          await registerTurn(nextTurnCount, nextDisc, x, y);
+        });
       }
 
-      boardElement.appendChild(squareElement)
-    })
-  })
+      boardElement.appendChild(squareElement);
+    });
+  });
+}
+
+// ターン(盤面)の状態を登録
+async function registerTurn(turnCount, disc, x, y) {
+  const requestBody = {
+    turnCount,
+    move: {
+      disc,
+      x,
+      y,
+    },
+  };
+
+  await fetch("/api/games/latest/turns", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(requestBody),
+  });
 }
 
 async function registerGame() {
-  const response = await fetch('/api/games', {
-    method: 'POST'
-  })
-  const game = await response.json()
-  console.log(game)
+  const response = await fetch("/api/games", {
+    method: "POST",
+  });
+  const game = await response.json();
+  console.log(game);
 }
 
 async function main() {
-  await showBoard()
+  await showBoard();
+  await registerGame();
 }
 
-main()
+main();
