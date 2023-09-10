7cd6aa8e4b2aff3e9eb4ccbfa4fb9bbb5dc8092c
tokatu4561 <ryobsk4561@gmail.com>
2023-09-10 12:45:33 +0900

    install mysql2 & api save new game

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import express from 'express';
 import morgan from 'morgan';
+import mysql from 'mysql2/promise';
 import 'express-async-errors'
 
 const PORT = 3000;
@@ -15,6 +16,26 @@ app.get('/api/hello', (req, res) => {
 
 app.get('/api/error', (req, res) => {
     throw new Error('Error!');
+})
+
+// post /api/games -> create a new game
+app.post('/api/games', async (req, res) => {
+    const startedAt = new Date();
+
+    const conn = await mysql.createConnection({
+        host: 'localhost',
+        database: 'reversi',
+        user: 'test',
+        password: 'password'
+    });
+
+    try {
+        await conn.execute('INSERT INTO games (started_at) VALUES (?)', [startedAt]);
+    } finally{
+        await conn.end();
+    }
+
+    res.json({ message: 'Game created' });
 })
 
 app.use((err: Error, req: express.Request, res: express.Response, next: express.NextFunction) => {
